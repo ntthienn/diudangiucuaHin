b57e639089a73dd5ea890d41035ffe5927ee0169
ntthienn <86788266+ntthienn@users.noreply.github.com>
2021-07-01 22:10:35 +0700

    Hinnee.iuthunnnn

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+    introTitle: 'Heluuu Lạc!',
+    introDesc: `Trên thế giới có hơn 7 tỷ nụ cười
+           Nhưng mà nụ cười của Bún nà đẹp nhất 🙊`,
+    btnIntro: 'hihi'
+    title: 'Chắc đang tủm tỉm cười đây mà 😌 nghiêm túc điiii',
+    desc: 'Bún xuynk đúng khumm 👀 ',
+    btnYes: 'Lại là điều hiển nhiên 😤',
+    btnNo: 'Không, Bún ạ :3',
+    question:'Trên thế giới hơn 7 tỉ người...., điền vào dấu ... câu trả lời thích hợp',
+    btnReply: 'Viết cho đúng vàoo biết chưa 🤬',
+    reply: 'Nhưng mà Bún đáng iu nhất 😚',
+    mess: 'Bún biết mà 😌. Điều quá nà hiển nhiên lunn',
+    messDesc: 'Đến đoạn này chắc đang cười ngoạc mồm ra rồi đúng khongg 😌 ',
+    btnAccept: 'Đúng lun.',
+    
