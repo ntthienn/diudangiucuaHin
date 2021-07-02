a24e123cefe2a58731480a2fe35ac4d7f081f986
ntthienn <86788266+ntthienn@users.noreply.github.com>
2021-07-02 17:50:21 +0700

    ThudangiucuaHin

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Rang Lạc hỏooo!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+              Mà sao Bún vẫn cutee nhìiiii 🥺`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    title: 'Chắc đang cười tủm tỉm rồi đây nhưng nghiêm túc này 😙',
+    desc: 'Bún xuynh đúng khumm 🙊 ',
+    btnYes: 'Ơ điều hiển nhiên còn gì 😑',
+    btnNo: 'Không, Bún aaaa :3',
+    question:'Trên thế giới có hơn 7 tỷ người nhưng..., điền vào dấu 3 chấm',
+    btnReply: 'Viết cho chuẩn vào 🤬',
+    reply: 'Bún lúc nào cũng đáng iuuuu 😚',
+    mess: 'Biết mà 😌 choii oiii',
+    messDesc: 'Đánh nhau hongg 😤',
+    btnAccept: 'Zô 😤',
+    messLink: 'https://giphy.com/gifs/upset-slapping-c4t11obaChpu0' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
